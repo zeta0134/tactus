9331b49f9fd1e1847d8d00974ef58bd9f185642a
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-10-15 16:49:07 -0400

    Initialize CHR RAM with some very basic BG tiles

--- prg/far_call.s
+++ prg/far_call.s
@@ -1,4 +1,5 @@
         .setcpu "6502"
+        .include "action53.inc"
         .include "far_call.inc"
         .include "nes.inc"
 
@@ -10,7 +11,28 @@ JumpTarget: .word $0000
         .segment "PRGFIXED_C000"
 
 .proc launch_far_call
-        ; crash on purpose
-        brk
-        rts ; never reached
+        ; preserve the current bank
+        lda CurrentBank
+        pha
+
+        a53_set_prg TargetBank
+
+        lda TargetBank
+        sta CurrentBank
+        
+        ; setup indirect jump to the far call address
+        lda #>(return_from_indirect-1)
+        pha
+        lda #<(return_from_indirect-1)
+        pha
+        jmp (JumpTarget)
+return_from_indirect:
+        ; (rts removes return address)
+        ; restore the original bank
+        pla
+        sta CurrentBank
+        a53_set_prg CurrentBank
+
+finished:
+        rts
 .endproc
