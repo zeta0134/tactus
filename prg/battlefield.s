0332f6874291bf0ace58367b99722014add9e4cb
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-10-15 23:08:05 -0400

    Hook up most of the battlefield update routine, and disco tiles
    Hot dang, this came together *fast.* Still a ton of work to do,
    but the basic skeleton is now complete and the technique proven.
    Next task is almost certainly going to be the player movement
    and start-of-beat delay, based on their inputs

--- prg/battlefield.s
+++ prg/battlefield.s
@@ -2,6 +2,7 @@
 
         .include "action53.inc"
         .include "battlefield.inc"
+        .include "enemies.inc"
         .include "far_call.inc"
         .include "chr.inc"
         .include "nes.inc"
@@ -12,26 +13,17 @@
 
 .segment "RAM"
 
-BATTLEFIELD_WIDTH = 14
-BATTLEFIELD_HEIGHT = 10
-BATTLEFIELD_SIZE = (BATTLEFIELD_WIDTH*BATTLEFIELD_HEIGHT)
-MAXIMUM_QUEUE_SIZE = 144
-
 battlefield: .res ::BATTLEFIELD_SIZE
 tile_data_1: .res ::BATTLEFIELD_SIZE
 tile_data_2: .res ::BATTLEFIELD_SIZE
-active_queue: .res ::BATTLEFIELD_HEIGHT
-inactive_queue: .res ::BATTLEFIELD_HEIGHT
+active_tile_queue: .res ::BATTLEFIELD_HEIGHT
+inactive_tile_queue: .res ::BATTLEFIELD_HEIGHT
+active_attribute_queue: .res (::BATTLEFIELD_HEIGHT / 2)
+inactive_attribute_queue: .res (::BATTLEFIELD_HEIGHT / 2)
 queued_bytes_counter: .res 1
 active_battlefield: .res 1
 
 .segment "PRG0_8000"
-
-TILE_REGULAR_FLOOR = $80
-TILE_DISCO_FLOOR =   $82
-TILE_WALL_TOP =      $84
-TILE_WALL_FACE =     $86
-TILE_PIT_EDGE =      $88
 
 
 test_layout:
@@ -40,6 +32,8 @@ FL := TILE_REGULAR_FLOOR
 WF := TILE_WALL_FACE
 WT := TILE_WALL_TOP
 PE := TILE_PIT_EDGE
+PT := TILE_PIT
+CF := TILE_CLOCK_FACE
         ;      0   1   2   3   4   5   6   7   8   9  10  11  12  13
         .byte WT, WT, WT, WT, WT, WT, WT, WT, WT, WT, WT, WT, WT, WT ; 0
         .byte WT, WF, WF, WF, WF, WF, WF, WF, WF, WF, WF, WF, WF, WT ; 1
@@ -48,9 +42,9 @@ PE := TILE_PIT_EDGE
         .byte WT, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, WT ; 4
         .byte WT, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, WT ; 5
         .byte WT, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, WT ; 6
-        .byte WT, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, WT ; 7
-        .byte WT, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, FL, WT ; 8
-        .byte WF, PE, PE, PE, PE, PE, PE, PE, PE, PE, PE, PE, PE, WF ; 9
+        .byte WT, FL, FL, FL, FL, PE, PE, PE, FL, FL, FL, FL, FL, WT ; 7
+        .byte WT, FL, FL, FL, FL, PT, PT, PT, FL, FL, FL, FL, FL, WT ; 8
+        .byte WF, PE, PE, PE, PE, PT, CF, PT, PE, PE, PE, PE, PE, WF ; 9
 .endscope
 
 .proc FAR_initialize_battlefield
@@ -75,11 +69,19 @@ loop:
 .proc reset_inactive_queue
         lda #1
         ldy #0
-loop:
-        sta inactive_queue, y
+tile_loop:
+        sta inactive_tile_queue, y
         iny
         cpy #::BATTLEFIELD_HEIGHT
-        bne loop
+        bne tile_loop
+
+        ldy #0
+attribute_loop:
+        sta inactive_attribute_queue, y
+        iny
+        cpy #(::BATTLEFIELD_HEIGHT / 2)
+        bne attribute_loop
+
         rts
 .endproc
 
@@ -88,14 +90,26 @@ loop:
         ; rationalle: anything we didn't get around to updating still needs to be drawn, we'll
         ; just be drawing it late with a visible glitch. It's fine
         ldy #0
-loop:
-        lda inactive_queue, y
-        sta active_queue, y
+tile_loop:
+        lda inactive_tile_queue, y
+        sta active_tile_queue, y
         iny
         cpy #::BATTLEFIELD_HEIGHT
-        bne loop
+        bne tile_loop
+
+        ldy #0
+attribute_loop:
+        lda inactive_attribute_queue, y
+        sta active_attribute_queue, y
+        iny
+        cpy #(::BATTLEFIELD_HEIGHT / 2)
+        bne attribute_loop
+
         ; now reset the inactive queue, setting it up for a full draw
         jsr reset_inactive_queue
+        lda active_battlefield
+        eor #%00000001
+        sta active_battlefield
         rts
 .endproc
 
@@ -104,6 +118,8 @@ loop:
         sta queued_bytes_counter
         jsr queue_active_tiles
         jsr queue_inactive_tiles
+        jsr queue_active_attributes
+        jsr queue_inactive_attributes
         rts
 .endproc
 
@@ -117,21 +133,21 @@ RowCounter := R5
         sta CurrentTile
         lda active_battlefield
         bne second_nametable
-        st16 NametableAddr, $2000
+        st16 NametableAddr, $2082
         jmp row_loop
 second_nametable:
-        st16 NametableAddr, $2400
+        st16 NametableAddr, $2482
 row_loop:
         lda queued_bytes_counter
-        cmp #MAXIMUM_QUEUE_SIZE
+        cmp #(MAXIMUM_QUEUE_SIZE - 28 - 28)
         bcc continue
         rts ; the queue is full; bail immediately
 continue:
         ldx CurrentRow
-        lda active_queue, x
+        lda active_tile_queue, x
         jeq skip
         lda #0
-        sta active_queue, x
+        sta active_tile_queue, x
 
         jsr _queue_tiles_common
         jmp converge
@@ -163,21 +179,21 @@ RowCounter := R5
         sta CurrentTile
         lda active_battlefield
         beq second_nametable
-        st16 NametableAddr, $2000
+        st16 NametableAddr, $2082
         jmp row_loop
 second_nametable:
-        st16 NametableAddr, $2400
+        st16 NametableAddr, $2482
 row_loop:
         lda queued_bytes_counter
-        cmp #MAXIMUM_QUEUE_SIZE
+        cmp #(MAXIMUM_QUEUE_SIZE - 28 - 28)
         bcc continue
         rts ; the queue is full; bail immediately
 continue:
         ldx CurrentRow
-        lda inactive_queue, x
+        lda inactive_tile_queue, x
         jeq skip
         lda #0
-        sta inactive_queue, x
+        sta inactive_tile_queue, x
 
         jsr _queue_tiles_common
         jmp converge
@@ -236,7 +252,7 @@ bottom_row_loop:
         ; bottom left
         lda battlefield, x
         clc
-        adc #16
+        adc #2
         sta VRAM_TABLE_START, y
         iny
         ; bottom right
@@ -255,6 +271,216 @@ bottom_row_loop:
         clc
         lda queued_bytes_counter
         adc #(28 + 28)
+        sta queued_bytes_counter
+
+        rts
+.endproc
+
+.proc queue_active_attributes
+CurrentRow := R0
+NametableAddr := R2
+CurrentTile := R4
+RowCounter := R5
+        lda #0
+        sta CurrentRow
+        sta CurrentTile
+        lda active_battlefield
+        bne second_nametable
+        st16 NametableAddr, $23C8
+        jmp row_loop
+second_nametable:
+        st16 NametableAddr, $27C8
+row_loop:
+        lda queued_bytes_counter
+        cmp #(MAXIMUM_QUEUE_SIZE - 8)
+        bcc continue
+        rts ; the queue is full; bail immediately
+continue:
+        ldx CurrentRow
+        lda active_attribute_queue, x
+        jeq skip
+        lda #0
+        sta active_attribute_queue, x
+
+        jsr _queue_attributes_common
+        jmp converge
+
+skip:
+        add16b NametableAddr, #8
+converge:
+        clc
+        lda CurrentTile
+        adc #(::BATTLEFIELD_WIDTH * 2)
+        sta CurrentTile
+        inc CurrentRow
+        lda CurrentRow
+        cmp #(::BATTLEFIELD_HEIGHT / 2)
+        beq done
+        jmp row_loop
+
+done:
+        rts
+.endproc
+
+.proc queue_inactive_attributes
+CurrentRow := R0
+NametableAddr := R2
+CurrentTile := R4
+RowCounter := R5
+        lda #0
+        sta CurrentRow
+        sta CurrentTile
+        lda active_battlefield
+        beq second_nametable
+        st16 NametableAddr, $23C8
+        jmp row_loop
+second_nametable:
+        st16 NametableAddr, $27C8
+row_loop:
+        lda queued_bytes_counter
+        cmp #(MAXIMUM_QUEUE_SIZE - 8)
+        bcc continue
+        rts ; the queue is full; bail immediately
+continue:
+        ldx CurrentRow
+        lda inactive_attribute_queue, x
+        jeq skip
+        lda #0
+        sta inactive_attribute_queue, x
+
+        jsr _queue_attributes_common
+        jmp converge
+
+skip:
+        add16b NametableAddr, #8
+converge:
+        clc
+        lda CurrentTile
+        adc #(::BATTLEFIELD_WIDTH * 2)
+        sta CurrentTile
+        inc CurrentRow
+        lda CurrentRow
+        cmp #(::BATTLEFIELD_HEIGHT / 2)
+        beq done
+        jmp row_loop
+
+done:
+        rts
+.endproc
+
+.proc _queue_attributes_common
+CurrentRow := R0
+NametableAddr := R2
+CurrentTile := R4
+RowCounter := R5
+AttributeByte := R6
+
+        write_vram_header_ptr NametableAddr, #8, VRAM_INC_1
+        ldx CurrentTile
+        ldy VRAM_TABLE_INDEX
+
+        ; left bookend, the left attributes here are always %00
+        ; top left
+        clc
+        ror AttributeByte
+        clc
+        ror AttributeByte
+        ; top right
+        lda battlefield, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; bottom left
+        clc
+        ror AttributeByte
+        clc
+        ror AttributeByte
+        ; bottom right
+        lda battlefield + ::BATTLEFIELD_WIDTH, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        lda AttributeByte
+        sta VRAM_TABLE_START, y
+        iny
+
+        inx
+
+        ; inner loop, repeat this 6 times for tiles 01 - 12
+        lda #6
+        sta RowCounter
+inner_loop:
+        ; top left
+        lda battlefield, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; top right
+        lda battlefield + 1, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; bottom left
+        lda battlefield + ::BATTLEFIELD_WIDTH, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; bottom right
+        lda battlefield + ::BATTLEFIELD_WIDTH + 1, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+
+        lda AttributeByte
+        sta VRAM_TABLE_START, y
+        iny
+        inx
+        inx
+        dec RowCounter
+        bne inner_loop
+
+        ; right bookend, here the rightmost tiles are always %00
+        ; top left
+        lda battlefield, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; top right
+        clc
+        ror AttributeByte
+        clc
+        ror AttributeByte
+        ; bottom left
+        lda battlefield + ::BATTLEFIELD_WIDTH, x
+        lsr
+        ror AttributeByte
+        lsr
+        ror AttributeByte
+        ; bottom right
+        clc
+        ror AttributeByte
+        clc
+        ror AttributeByte
+
+        lda AttributeByte
+        sta VRAM_TABLE_START, y
+        iny
+
+        sty VRAM_TABLE_INDEX
+        inc VRAM_TABLE_ENTRIES
+        
+        add16b NametableAddr, #8
+
+        clc
+        lda queued_bytes_counter
+        adc #(8)
         sta queued_bytes_counter
 
         rts
