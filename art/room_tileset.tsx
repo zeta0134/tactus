88b07f6151055bc62fc8770c2d6c5c08e6b12991
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-10-30 10:44:58 -0400

    Add a python script to automate layout creation from Tiled

---
+++ art/room_tileset.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="room_tiles" tilewidth="16" tileheight="16" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="enemy_index" type="int" value="128"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/floor.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="enemy_index" type="int" value="132"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/disco_floor.png"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="enemy_index" type="int" value="136"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/wall_face.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="enemy_index" type="int" value="140"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/wall_top.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="enemy_index" type="int" value="144"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/pit_edge.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="enemy_index" type="int" value="148"/>
+  </properties>
+  <image width="16" height="16" source="static_tiles/pit_center_technical.png"/>
+ </tile>
+</tileset>
