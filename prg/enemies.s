a1aacc3f1228881df7c45ef6783afb8049c791eb
Nicholas Flynt <thegamefreak0134@gmail.com>
2022-10-31 21:42:29 -0400

    Do not draw disco tiles while the room is cleared

--- prg/enemies.s
+++ prg/enemies.s
@@ -356,7 +356,12 @@ sprite_failed:
 .proc draw_disco_tile
 CurrentRow := R14
 CurrentTile := R15
-        ; TODO: maybe if the player's combo gauge is empty, we don't do the disco thing?
+        ; If the current room is cleared, we release the player from the perils of the tempo. In
+        ; this case, the disco tiles look _very wrong_, so don't draw them.
+        ldx PlayerRoomIndex
+        lda room_flags, x
+        and #ROOM_FLAG_CLEARED
+        bne regular_tile
 
         ; here we want to draw a checkerboard pattern, which alternates every time the beat advances
         ; we can do this with an XOR of these low bits: X coordinate, Y coordinate, Beat Counter
@@ -366,11 +371,11 @@ CurrentTile := R15
         and #%00000001
         bne disco_tile
 regular_tile:
-        lda #TILE_DISCO_FLOOR
+        lda #TILE_REGULAR_FLOOR
         jsr draw_tile_here
         rts
 disco_tile:
-        lda #TILE_REGULAR_FLOOR
+        lda #TILE_DISCO_FLOOR
         jsr draw_tile_here
         rts
 .endproc
