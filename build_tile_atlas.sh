7e7661ce9758364c6983e232696b9485f3b85628
Zeta <zeta0134@reploid.cafe>
2024-08-29 13:00:30 -0400

    Fix up dust and spore tiles with the new disco logic

--- build_tile_atlas.sh
+++ build_tile_atlas.sh
@@ -2,3 +2,5 @@
 
 tools/build_map_tiles.py art/tilesets/disco_floor_tiles.png art/disco_tiles
 tools/build_map_tiles.py art/tilesets/map_tiles.png art/map_tiles
+tools/build_map_tiles.py art/tilesets/dust_tiles.png art/background_tiles
+tools/build_map_tiles.py art/tilesets/spore_tiles.png art/background_tiles
