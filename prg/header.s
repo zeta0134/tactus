989a436edf52343ec00528a8945bdc69931ef78d
Zeta <zeta0134@reploid.cafe>
2024-12-11 16:06:27 -0500

    Add a bunch of particles, bump to 2.0 MiB CHR ROM, etc.
    We aren't using the second 1.0 MiB yet, but it's time to start
    working on that part

--- prg/header.s
+++ prg/header.s
@@ -6,10 +6,10 @@
 .segment "HEADER"
         .byte "NES", $1a
         .byte $40               ; 64x 16KB PRG-ROM banks = 1024 KB total
-        .byte $80               ; 128x 8KB CHR-ROM banks = 1024 KB total
+        .byte $00               ; 256x 8KB CHR-ROM banks = 2048 KB total
         .byte $A2, $A8          ; Mapper 682 (Rainbow) (also iNes 2.0 specifier)
         .byte $02               ; 
-        .byte $00               ;
+        .byte $10               ; MSB of CHR-ROM (256k)
         ;.byte $B0               ; PRG-NVRAM: 128k (battery backed)
         .byte $90               ; PRG-NVRAM: 32k (battery backed)
         .byte $00               ; CHR-RAM: 32k (shift count: 9)
