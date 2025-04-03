d5f68271f1d25842d615abb802530ba33ce22319
Zeta <zeta0134@reploid.cafe>
2025-04-03 03:01:23 -0400

    Implement the freeze mechanic. Hrm... this one needs juice.

--- prg/player.s
+++ prg/player.s
@@ -1034,6 +1034,7 @@ player_state_lut:
         .word player_state_casting
         .word player_state_dead
         .word player_state_shocked
+        .word player_state_frozen
 
 ; Called once at the beginning of every beat
 .proc FAR_update_player
@@ -1441,11 +1442,12 @@ TargetCol := R15
         ; Reset to our idle sprite, which may get replaced by taking damage
         ldx PlayerSpriteIndex
         set_player_sprite_x SPRITE_PLAYER_01_PLAYER
-        jmp done_setting_initial_sprite
+        ; Now just BECOME the normal state, and skip all this other nonsense. This way if the player
+        ; queues up an action on the frame they would recover, we honor that action.
+        jmp player_state_normal
 stun_not_expired:
         ldx PlayerSpriteIndex
         set_player_sprite_x SPRITE_PLAYER_02_PLAYER_STUN
-done_setting_initial_sprite:
 
         lda #0
         sta PlayerTookDamageThisBeat
@@ -1477,6 +1479,141 @@ no_damage_taken:
 
         ; Update the player's combo counter
         jsr update_chain_and_combo
+
+        ; Now we may finalize the player's position and draw
+        lda TargetRow
+        sta PlayerRow
+        lda TargetCol
+        sta PlayerCol
+
+        jsr set_player_target_coordinates
+
+        ; If the player is still holding this directional input, carry it over
+        ; to the next beat as a held input
+        ; (note: do this part unconditionally, as some mechanics rely on held inputs.
+        ; the optional ones will be checked at each site)
+        ldx PlayerNextDirection
+        lda player_direction_button_lut, x
+        and ButtonsThisFrame
+        beq done_with_held_inputs
+        lda PlayerNextDirection
+        sta PlayerHeldDirection
+done_with_held_inputs:
+        ; Clear player intent for the next beat
+        lda #0
+        sta PlayerNextDirection
+
+        jsr apply_player_torchlight
+
+        ; Detect exits and, if necessary, transition to the next room
+        jsr detect_exit
+
+        ; Detect being dead and, if necessary, transition to the end screen
+        jsr detect_critical_existence_failure
+
+        ; (Notably: do not detect pausing or spellcasting. Holding a bomb overrides both!)
+
+        lda #0
+        sta PlayerIntendsToBomb
+        sta PlayerIntendsToCast
+        sta PlayerIntendsToWait
+        sta PlayerIntendsToPause
+
+        perform_zpcm_inc
+
+        ; If necessary, cleanup dialog states through movement
+        jsr cleanup_dialog_state
+
+        ; Finally, our position is finalized, so compute the lookup table ptr for distance
+        ; (this massively improves enemy AI during pathfinding)
+        near_call FAR_compute_player_distance_lut_ptr
+        rts
+.endproc
+
+; A state during which we are frozen solid! A few different conditions will cause this state
+; to revert almost right away, but otherwise, we the player must "tap" the ice to decrease
+; the counter. This will need juice so the player has appropriate feedback, etc.
+.proc player_state_frozen
+TorchlightTotal := R0
+
+PlayerSquare := R2
+
+TargetRow := R14
+TargetCol := R15
+
+        ; If the player intends to move, then process the effect expiry as usual
+        lda PlayerNextDirection
+        ora PlayerHeldDirection
+        beq no_movement_attempted
+        jsr process_lingering_effect_expiry
+        ; TODO: if we're going to juice up the tap with player shake or SFX, do that here!
+no_movement_attempted:
+
+        ; If we're in a "hot" room, defrost ourselves instantly
+        ldx PlayerRoomIndex
+        lda room_palette_variant, x
+        cmp #ROOM_PALETTE_FIRE
+        bne no_defrosting_today
+        lda #PLAYER_STATUS_NORMAL
+        sta PlayerLingeringStatusType
+        lda #0
+        sta PlayerLingeringStatusDuration
+        sta PlayerLingeringStatusFrame
+        ; TODO: if we're going to juice up the defrosting action, do that here
+no_defrosting_today:
+
+        ; TODO: for game feel reasons, should we **process** the normal state here? If we don't
+        ; then on the beat the player recovers, they'll still be able to take no action...
+        ; get this actually happening in a combat situation, then revisit!
+
+        ; If the effect has expired, go ahead and revert us back to normal state
+        lda PlayerLingeringStatusType
+        bne stun_not_expired
+        ; Reset to the normal state then
+        lda #PLAYER_STATE_NORMAL
+        sta PlayerState
+        lda #0
+        sta PlayerBeatsInThisState
+        ; Reset to our idle sprite, which may get replaced by taking damage
+        ldx PlayerSpriteIndex
+        set_player_sprite_x SPRITE_PLAYER_01_PLAYER
+        ; Now just BECOME the normal state, and skip all this other nonsense. This way if the player
+        ; queues up an action on the frame they would recover, we honor that action.
+        jmp player_state_normal
+stun_not_expired:
+        ldx PlayerSpriteIndex
+        set_player_sprite_x SPRITE_PLAYER_02_PLAYER_STUN
+
+        lda #0
+        sta PlayerTookDamageThisBeat
+        sta PlayerDamageAnimCounter
+        ; If no damage direction is set, default to a kinda random-circle-y lookin' thing.
+        sta PlayerIncomingDamageDirection
+
+        ; Every beat we'll by default be in our generic palette. We need to recover from whatever
+        ; the previous beat's effect was doing, so flag that here.
+        lda #1
+        sta StagingObjPaletteDirty
+
+resolve_enemy_collision:
+        lda PlayerRow
+        sta TargetRow
+        lda PlayerCol
+        sta TargetCol
+        near_call FAR_player_resolve_collision
+
+        ; If we just took damage, then clear the stun state. (We might also be dead, we'll check for that in a minute.)
+        lda PlayerTookDamageThisBeat
+        beq no_damage_taken
+        ; Revert us to our normal state, but don't set a sprite (the damage routine did that)
+        lda #PLAYER_STATE_NORMAL
+        sta PlayerState
+        lda #0
+        sta PlayerBeatsInThisState
+no_damage_taken:
+
+        ; DO NOT update the player's chain/combo counter. We are frozen! 
+        ; (This unusual mechanic might be fun to exploit for puzzles.)
 
         ; Now we may finalize the player's position and draw
         lda TargetRow
@@ -2797,8 +2934,6 @@ sprite_failed:
         bne processing_required
         rts ; bail fast if there is nothing to do
 processing_required:
-        cmp #PlAYER_STATUS_FROZEN
-        beq player_is_frozen
         ; Normal expiry
 normal_expiry:
         dec PlayerLingeringStatusDuration
@@ -2812,10 +2947,5 @@ cancel_effect:
         lda #0
         sta PlayerLingeringStatusDuration
         sta PlayerLingeringStatusFrame
-        rts
-player_is_frozen:
-        ; Breaking out of freezing has custom logic in the respective stun
-        ; state, which will handle terminating the effect. Take no automatic
-        ; action. (This shouldn't be reached, actually.)
         rts
 .endproc
